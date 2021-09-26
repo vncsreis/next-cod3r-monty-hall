25b5f729fa6cdbca0a8d4499041a44a4b338b844
Vinícius <euphonion@tutanota.com>
2021-09-26 15:41:06 -0300

    Commit inicial + componente presente

---
+++ components/Presente/index.jsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/Presente.module.css';
+
+export default function Presente() {
+  return (
+    <div className={styles.presente}>
+      <div className={styles.tampa} />
+      <div className={styles.laco1} />
+      <div className={styles.laco2} />
+      <div className={styles.corpo} />
+    </div>
+  );
+}
